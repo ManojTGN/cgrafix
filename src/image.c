55758e49951b76b745d9d8a6294bb89d3ed525a6
ManojTGN <manoj.thunderviz@gmail.com>
2023-04-13 22:44:25 +0530

    update image
    add image rotation (bugged)

--- src/image.c
+++ src/image.c
@@ -10,7 +10,7 @@ int createGrafixImage(grafixImage *image, const char* filePath, int fileType){
         setGrafixError("grafixError:Unable To Load Image;Reason:No File Found");
         return 0;
     }
-
+    
     image->height = 0;
     image->width = 0;
     fseek(image_file, 0x0012, SEEK_SET);
@@ -33,7 +33,7 @@ int createGrafixImage(grafixImage *image, const char* filePath, int fileType){
     for (int x = 0; x < image->width; x++) {
         
         if(fread(pixel, sizeof(unsigned char), pixelSize, image_file) != pixelSize) break;
-
+        
         index = ( x + y*image->width ) *3;
         image->_frame[ index+0 ] = pixel[0];
         image->_frame[ index+1 ] = pixel[1];
@@ -62,6 +62,8 @@ void drawGrafixImage(grafixWindow window, grafixImage image, int x, int y){
     for (int _x = x; _x < x+image.width; _x++) {
     for (int _y = y; _y < y+image.height ; _y++) {
         
+        if(_x < 0 || _y < 0 || _x >= window.width || y >= window.height) continue;
+
         index = (_x + _y * window.width) * 3;
         imageIndex = ( (_x-x) + (_y-y)*image.width) * 3;
         BUFFERS[window.id].frameBuffer[index] = image._frame[imageIndex];
@@ -70,5 +72,77 @@ void drawGrafixImage(grafixWindow window, grafixImage image, int x, int y){
 
     }
     }
+
+}
+
+void rotateGrafixImage(grafixImage *image, float angle){
+    
+    int padding = ceil(sqrt(image->width * image->width + image->height * image->height)) - (image->width + image->height) / 2;
+    int padded_w = image->width + 2 * padding;
+    int padded_h = image->height + 2 * padding;
+
+    unsigned char* temp_buffer = (unsigned char*)malloc(padded_w * padded_h * 3 * sizeof(unsigned char));
+
+    float cos_theta = cos(angle);
+    float sin_theta = sin(angle);
+    float matrix[] = { cos_theta, -sin_theta, 0, sin_theta, cos_theta, 0, 0, 0, 1 };
+
+    float inv_matrix[] = { cos_theta, sin_theta, 0, -sin_theta, cos_theta, 0, 0, 0, 1 };
+
+    for (int y = -padding; y < image->height + padding; y++) {
+        for (int x = -padding; x < image->width + padding; x++) {
+
+            float xf = x * matrix[0] + y * matrix[1] + (image->width / 2) * (1 - matrix[0]) - (image->height / 2) * matrix[1];
+            float yf = x * matrix[3] + y * matrix[4] + (image->height / 2) * (1 - matrix[4]) - (image->width / 2) * matrix[3];
+
+            int ix = (int)round(xf);
+            int iy = (int)round(yf);
+
+            float dx = xf - ix;
+            float dy = yf - iy;
+
+            if (ix < 0) ix = 0;
+            if (iy < 0) iy = 0;
+            if (ix >= image->width - 1) ix = image->width - 2;
+            if (iy >= image->height - 1) iy = image->height - 2;
+
+            int idx = (iy * padded_w + ix) * 3;
+            float r = temp_buffer[idx] * (1 - dx) * (1 - dy);
+            float g = temp_buffer[idx + 1] * (1 - dx) * (1 - dy);
+            float b = temp_buffer[idx + 2] * (1 - dx) * (1 - dy);
+
+            idx = (iy * padded_w + ix + 1) * 3;
+            r += temp_buffer[idx] * dx * (1 - dy);
+            g += temp_buffer[idx + 1] * dx * (1 - dy);
+            b += temp_buffer[idx + 2] * dx * (1 - dy);
+
+            idx = ((iy + 1) * padded_w + ix) * 3;
+            r += temp_buffer[idx] * (1 - dx) * dy;
+            g += temp_buffer[idx + 1] * (1 - dx) * dy;
+            b += temp_buffer[idx + 2] * (1 - dx) * dy;
+
+            idx = ((iy + 1) * padded_w + ix + 1) * 3;
+            r += temp_buffer[idx] * dx * dy;
+            g += temp_buffer[idx + 1] * dx * dy;
+            b += temp_buffer[idx + 2] * dx * dy;
+
+            idx = ((y + padding) * padded_w + (x + padding)) * 3;
+            temp_buffer[idx] = (unsigned char)r;
+            temp_buffer[idx + 1] = (unsigned char)g;
+            temp_buffer[idx + 2] = (unsigned char)b;
+        }
+    }
+
+    for (int y = 0; y < image->height; y++) {
+        for (int x = 0; x < image->width; x++) {
+            int src_idx = ((y + padding) * padded_w + (x + padding)) * 3;
+            int dst_idx = (y * image->width + x) * 3;
+            image->_frame[dst_idx] = temp_buffer[src_idx];
+            image->_frame[dst_idx + 1] = temp_buffer[src_idx + 1];
+            image->_frame[dst_idx + 2] = temp_buffer[src_idx + 2];
+        }
+    }
+
+    free(temp_buffer);
 
 }
